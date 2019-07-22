a01d9510c6043ee8e0079cf32b4d34e631e7aa59
Student <student@Makerss-MacBook-Pro.local>
2019-07-22 16:01:13 +0100

    finish fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,4 +8,18 @@ describe("FizzBuzz", function() {
   it("should returns FizzBuzz if is divisible by 5 & 3", function() {
     expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
   });
+
+  it("should returns Fizz if is divisible by 3", function() {
+    expect(fizzBuzz.play(3)).toEqual('Fizz');
+  });
+
+  it("should returns Buzz if is divisible by 5", function() {
+    expect(fizzBuzz.play(5)).toEqual('Buzz');
+  });
+
+  it("should returns number if is not divisible by 5 or 3", function() {
+    expect(fizzBuzz.play(7)).toEqual(7);
+  });
+
+
 });
